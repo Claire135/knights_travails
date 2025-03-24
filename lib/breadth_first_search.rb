ef2cc03e754b8e4fd00f72dea25b2f1a637aab7c
ClaireCompare <claire@markcompare.com>
2025-03-24 13:07:53 +0100

    refactored as metrics were too long

--- lib/breadth_first_search.rb
+++ lib/breadth_first_search.rb
@@ -1,43 +1,52 @@
+# frozen_string_literal: true
+
 # defines a search algorhythm to find the quickest route
-# saves the path used.  
+# saves the path used.
 
 module BreadthFirstSearch
   def bfs(start_node, end_node)
     return [] if start_node.nil? || end_node.nil?
 
-    discovered_nodes = [start_node] 
+    parent = perform_bfs(start_node, end_node)
+    return nil unless parent
+
+    reconstruct_path(parent, end_node)
+  end
+
+  private
+
+  def perform_bfs(start_node, end_node)
+    discovered_nodes = [start_node]
     visited_nodes = Set.new([start_node])
-    parent = { start_node => nil } # Hash to store parent nodes to trace the route the node took
+    parent = { start_node => nil }
 
     until discovered_nodes.empty?
       current_node = discovered_nodes.shift
+      return parent if current_node == end_node
 
-      return reconstruct_path(parent, end_node) if current_node == end_node
-
-      current_node.connections.each do |neighbor|
-        unless visited_nodes.include?(neighbor)
-          visited_nodes.add(neighbor)
-          parent[neighbor] = current_node
-          discovered_nodes << neighbor
-        end
-      end
+      process_neighbors(current_node, discovered_nodes, visited_nodes, parent)
     end
-    nil  # Return nil if no path is found
+    nil
   end
 
+  def process_neighbors(current_node, discovered_nodes, visited_nodes, parent)
+    current_node.connections.each do |neighbor|
+      next if visited_nodes.include?(neighbor)
 
-    private
-    def reconstruct_path(parent, end_node)
-        path = []
-        current_node = end_node
-        
-        while current_node
-            path.unshift(current_node)
-            current_node = parent[current_node]
-        end
-        path
+      visited_nodes.add(neighbor)
+      parent[neighbor] = current_node
+      discovered_nodes << neighbor
     end
+  end
+
+  def reconstruct_path(parent, end_node)
+    path = []
+    current_node = end_node
+
+    while current_node
+      path.unshift(current_node)
+      current_node = parent[current_node]
+    end
+    path
+  end
 end
-    
-
-
